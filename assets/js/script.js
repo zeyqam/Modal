3bb8421eba4c976ca234c44086b271e356a39bd8
zeyqam <zegamda@code.edu.az>
2024-05-02 00:34:08 +0400

    homework

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+document.getElementById("openModal").addEventListener("click", function () {
+  document.getElementById("myModal").style.display = "block";
+});
+
+document
+  .getElementsByClassName("close")[0]
+  .addEventListener("click", function () {
+    document.getElementById("myModal").style.display = "none";
+  });
+
+window.addEventListener("click", function (event) {
+  if (event.target == document.getElementById("myModal")) {
+    document.getElementById("myModal").style.display = "none";
+  }
+});
